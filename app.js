24186a4a62f4f7264a76b51ee0d4c0d3ee748829
Aissam BELHACHMI <edmeral.aissam@gmail.com>
2014-01-30 20:47:13 +0000

    First version

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var ent = require('ent');
+
+
+var app = express();
+var server = require('http').createServer(app);
+
+server.listen(8080);
+
+app.use(express.static(__dirname + '/public'))
+.get('/', function(req, res) {
+	res.sendfile(__dirname + '/public/index.html');
+});
+
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function(socket) {
+	socket.on('new-user', function(pseudo) {
+		socket.set('pseudo', pseudo);
+		socket.broadcast.emit('new-user', ent.encode(pseudo));
+	});
+	socket.on('message', function(message) {
+		socket.get('pseudo', function(error, pseudo) {
+			socket.broadcast.emit('message', {
+				pseudo: ent.encode(pseudo),
+				message: ent.encode(message)
+			});
+		});
+	});
+});
---
+++ public/script.js
@@ -0,0 +1,39 @@
+var pseudo = prompt('Choose a pseudo') || 'Anonymous';
+var htmlEscapes = {
+	'&': '&amp;',
+	'<': '&lt;',
+	'>': '&gt;',
+	'"': '&quot;',
+	"'": '&#x27;',
+	'/': '&#x2F;'
+};
+var escape = function(string) {
+	return ('' + string).replace(htmlEscaper, function(match) {
+		return htmlEscapes[match];
+	});
+};
+var htmlEscaper = /[&<>"'\/]/g;
+var socket = io.connect('http://localhost');
+
+
+socket.emit('new-user', pseudo);
+
+socket.on('new-user', function(pseudo) {
+	$('#conversation').prepend('<p><em><strong>' + pseudo + '</strong>  has joined the chat.</em><p><hr>');
+});
+socket.on('message', function(data) {
+	$('#conversation').prepend('<p class="name">' + data.pseudo + '</p><p class="message">' + data.message + '</p><hr>');
+});
+
+var input = $('input');
+input.keyup(function(event) {
+	if (event.which == 13) {
+		if (input.val()) {
+			socket.emit('message', input.val());
+			$('#conversation').prepend('<p class="name">' + pseudo + '</p><p class="message">' + escape(input.val()) + '</p><hr>');
+			input.val('');
+			input.attr('placeholder', 'Type your message...');
+			input.focus();
+		}
+	}
+});
