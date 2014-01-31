e1c5db240b61334fc1a7063d25ac9cf48050a89c
Aissam BELHACHMI <edmeral.aissam@gmail.com>
2014-01-31 18:12:12 +0000

    Added animations

--- public/script.js
+++ public/script.js
@@ -19,10 +19,10 @@ var socket = io.connect('http://simple-chat-room.herokuapp.com');
 socket.emit('new-user', pseudo);
 
 socket.on('new-user', function(pseudo) {
-	$('#conversation').prepend('<p><em><strong>' + pseudo + '</strong>  has joined the chat.</em><p><hr>');
+	$('#conversation').prepend('<div class="animated fadeInLeft"><p><em><strong>' + pseudo + '</strong>  has joined the chat.</em><p><hr></div>');
 });
 socket.on('message', function(data) {
-	$('#conversation').prepend('<p class="name he">' + data.pseudo + '</p><p class="message he">' + data.message + '</p><hr>');
+	$('#conversation').prepend('<div class="animated fadeInLeft"><p class="name he">' + data.pseudo + '</p><p class="message he">' + data.message + '</p><hr></div>');
 });
 
 var input = $('input');
@@ -30,7 +30,7 @@ input.keyup(function(event) {
 	if (event.which == 13) {
 		if (input.val()) {
 			socket.emit('message', input.val());
-			$('#conversation').prepend('<p class="name">' + pseudo + '</p><p class="message">' + escape(input.val()) + '</p><hr>');
+			$('#conversation').prepend('<div class="animated fadeInRight"><p class="name">' + pseudo + '</p><p class="message">' + escape(input.val()) + '</p><hr></div>');
 			input.val('');
 			input.attr('placeholder', 'Type your message...');
 			input.focus();
