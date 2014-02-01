377d1c75c735a6958c33c0ecfdc703a45f3e978c
Aissam BELHACHMI <edmeral.aissam@gmail.com>
2014-02-01 03:14:35 +0000

    Fixed a bug occuring when socket.io sends an error

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ var server = require('http').createServer(app);
 server.listen(process.env.PORT || 8080);
 
 app.use(express.static(__dirname + '/public'))
-.get('/', function(req, res) {
-	res.sendfile(__dirname + '/public/index.html');
-});
+	.get('/', function(req, res) {
+		res.sendfile(__dirname + '/public/index.html');
+	});
 
 var io = require('socket.io').listen(server);
 io.enable('browser client minification');
@@ -23,10 +23,15 @@ io.sockets.on('connection', function(socket) {
 	});
 	socket.on('message', function(message) {
 		socket.get('pseudo', function(error, pseudo) {
-			socket.broadcast.emit('message', {
-				pseudo: ent.encode(pseudo),
-				message: ent.encode(message)
-			});
+			if (error) {
+				console.log(error);
+			} 
+			else {
+				socket.broadcast.emit('message', {
+					pseudo: ent.encode(pseudo),
+					message: ent.encode(message)
+				});
+			}
 		});
 	});
 });
