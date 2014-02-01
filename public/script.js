33f25fe34b0188d735eea66c34294d47a8d8374b
Aissam BELHACHMI <edmeral.aissam@gmail.com>
2014-02-01 13:06:34 +0000

    Added some animations

--- public/script.js
+++ public/script.js
@@ -20,9 +20,21 @@ socket.emit('new-user', pseudo);
 
 socket.on('new-user', function(pseudo) {
 	$('#conversation').prepend('<div class="animated fadeInLeft"><p><em><strong>' + pseudo + '</strong>  has joined the chat.</em><p><hr></div>');
+	//Add animation
+	$('#conversation').addClass('animated fadeInDown');
+	//and then remove it
+	$('#conversation').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+		$('#conversation').removeClass('animated fadeInDown');
+	});
 });
 socket.on('message', function(data) {
 	$('#conversation').prepend('<div class="animated fadeInLeft"><p class="name he">' + data.pseudo + '</p><p class="message he">' + data.message + '</p><hr></div>');
+	//Add animation
+	$('#conversation').addClass('animated fadeInDown');
+	//and then remove it
+	$('#conversation').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+		$('#conversation').removeClass('animated fadeInDown');
+	});
 });
 
 var input = $('input');
@@ -31,6 +43,12 @@ input.keyup(function(event) {
 		if (input.val()) {
 			socket.emit('message', input.val());
 			$('#conversation').prepend('<div class="animated fadeInRight"><p class="name">' + pseudo + '</p><p class="message">' + escape(input.val()) + '</p><hr></div>');
+			//Add animation
+			$('#conversation').addClass('animated fadeInDown');
+			//and then remove it
+			$('#conversation').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+				$('#conversation').removeClass('animated fadeInDown');
+			});
 			input.val('');
 			input.attr('placeholder', 'Type your message...');
 			input.focus();
